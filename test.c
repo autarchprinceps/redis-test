c966e4732878d198dd7fa6b7e233da1f84070342
autarch princeps <autarch@outlook.com>
2013-12-29 20:55:32 +0100

    Unix Domain Socket

--- test.c
+++ test.c
@@ -18,15 +18,45 @@
 
 int main(int argc, char** argv) {
 	redisContext* c = redisConnect(argv[1], atoi(argv[2]));
+	if (c == NULL || c->err) {
+		if (c) {
+		    printf("Connection error: %s\n", c->errstr);
+		    redisFree(c);
+		} else {
+			printf("Connection error: can't allocate redis context\n");
+		}
+		exit(1);
+	}
 	redisReply* reply;
 	reply = redisCommand(c, "HGETALL %s", "test");
 	printf("test: %s\n", reply->str);
 	freeReplyObject(reply);
 	reply = redisCommand(c, "GET hello");
-	printf("Hello %s\n", reply->str);
+	printf("hello %s\n", reply->str);
 	freeReplyObject(reply);
 	reply = redisCommand(c, "GET hallo");
-	printf("Hallo %s\n", reply->str);
+	printf("hallo %s\n", reply->str);
+	freeReplyObject(reply);
+	redisFree(c);
+	printf("Unix Socket:\n");
+	c = redisConnectUnix("/var/run/redis/redis.sock");
+	if (c == NULL || c->err) {
+		if (c) {
+		    printf("Connection error: %s\n", c->errstr);
+		    redisFree(c);
+		} else {
+			printf("Connection error: can't allocate redis context\n");
+		}
+		exit(1);
+	}
+	reply = redisCommand(c, "HGETALL %s", "test");
+	printf("test: %s\n", reply->str);
+	freeReplyObject(reply);
+	reply = redisCommand(c, "GET hello");
+	printf("hello %s\n", reply->str);
+	freeReplyObject(reply);
+	reply = redisCommand(c, "GET hallo");
+	printf("hallo %s\n", reply->str);
 	freeReplyObject(reply);
 	redisFree(c);
 	return 0;
