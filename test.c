61c1f049338836db9a3d1f41883902a7a21e0635
autarch princeps <autarch@outlook.com>
2013-12-29 18:09:50 +0100

    Simple C file

---
+++ test.c
@@ -0,0 +1,32 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  test.c
+ *
+ *    Description:  
+ *
+ *        Created:  29.12.2013 17:35:24
+ *
+ *         Author:  autarch princeps (), autarch@outlook.com
+ *
+ * =====================================================================================
+ */
+#include <hiredis/hiredis.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char** argv) {
+	redisContext* c = redisConnect(argv[1], atoi(argv[2]));
+	redisReply* reply;
+	reply = redisCommand(c, "HGETALL %s", "test");
+	printf("test: %s\n", reply->str);
+	freeReplyObject(reply);
+	reply = redisCommand(c, "GET hello");
+	printf("Hello %s\n", reply->str);
+	freeReplyObject(reply);
+	reply = redisCommand(c, "GET hallo");
+	printf("Hallo %s\n", reply->str);
+	freeReplyObject(reply);
+	return 0;
+}
